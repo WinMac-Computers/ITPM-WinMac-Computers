fda6f9de9b6e6cc16682ca5ed778787a596b02d1
anjanawijesooriya <anjana.wijesooriya2000@gmail.com>
2022-04-05 23:23:53 +0530

    login implemented for both sides

---
+++ backend/controllers/Auth.js
@@ -0,0 +1,193 @@
+const User = require("../models/Auth");
+const SendEmail = require("../utilities/SendEmail");
+const crypto = require("crypto");
+const { once } = require("events");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const { use } = require("bcrypt/promises");
+
+//when we use asynchronous function we need try catch block
+exports.register = async (req, res) => {
+  //controller for register
+  const { username, email, password, type, dept } = req.body; //destructor method
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+      type,
+      dept, //this.password filled of user.js in models
+    });
+    sendToken(user, 200, res);
+  } catch (error) {
+    if (error.code === 11000) {
+      const message = "Already have an account using this email";
+      return res.status(400).json({ success: false, error: message });
+    }
+    if (error.name === "ValidationError") {
+      const message = Object.values(error.errors).map((val) => val.message);
+      return res.status(400).json({ success: false, error: message });
+    }
+  }
+};
+
+exports.login = async (req, res) => {
+  //controller for login
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    //backend validation
+    return res
+      .status(400)
+      .json({ success: false, error: "Please enter email and password" });
+  } //400 bad request
+
+  try {
+    const user = await User.findOne({ email }).select("+password"); //match two passwords
+
+    if (!user) {
+      //true
+      return res.status(401).json({
+        success: false,
+        available: "User does not exist, please create an account !",
+      });
+    }
+
+    const isMatch = await user.matchPasswords(password); //matching the passwords from the recieved and from the db
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid Credentials" });
+    }
+
+    sendToken(user, 200, res);
+  } catch (error) {
+    res.status(500).json({
+      //500 internal server error
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+exports.forgotPassword = async (req, res) => {
+  //controller for forgot password
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email }); //check for email availability for sending emails
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Email could not be sent" });
+    }
+
+    const resetToken = user.getResetPasswordToken(); //get the password reset token
+
+    await user.save();
+
+    const resetURL = `http://localhost:3000/passwordreset${resetToken}`; //setting a url to send emails for users
+
+    const message = `
+        <h1>Hello There!, You have requested a password reset</h1>
+        <p>Please click this url to reset your password 👇</p>
+        <a href=${resetURL} clicktracking=off>${resetURL}</a>
+        `;
+    try {
+      await SendEmail({
+        //send email
+        to: user.email,
+        subject: "Password Reset Request",
+        text: message,
+      });
+
+      res.status(200).json({ success: true, verify: "Email Sent" });
+    } catch (error) {
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+
+      await user.save();
+
+      return res
+        .status(500)
+        .json({ success: false, error: "Email could not be sent" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.resetpassword = async (req, res) => {
+  //controller for reset password
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex"); //create hash code using crypto
+
+  try {
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() }, //find and update the relevant database field
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid Reset Token" });
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ success: true, verify: "Password Reset Successfull" });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const message = Object.values(error.errors).map((val) => val.message);
+      return res.status(400).json({ success: false, error: message });
+    }
+  }
+};
+
+//when we use asynchronous message we need try catch block
+exports.registerStaff = async (req, res) => {
+  //controller for register
+  const { username, email, password, type, dept } = req.body; //destructure method
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+      type,
+      dept, //this.password filled of user.js in models
+    });
+    sendToken(user, 200, res);
+  } catch (error) {
+    if (error.code === 11000) {
+      const message = "Already have an account using this email";
+      return res.status(400).json({ success: false, error: message });
+    }
+    if (error.name === "ValidationError") {
+      const message = Object.values(error.errors).map((val) => val.message);
+      return res.status(400).json({ success: false, error: message });
+    }
+  }
+};
+
+const sendToken = (user, statusCode, res) => {
+    //JWT get
+    const token = user.getSignedToken();
+    const username = user.username;
+    const email = user.email;
+    const type = user.type;
+    const dept = user.dept;
+    res.status(200).json({ success: true, token, username, email, type, dept });
+};
