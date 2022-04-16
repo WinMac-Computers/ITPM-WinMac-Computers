9d3e4c09481a19dd92bd41970b81269a7a06bfaa
anjanawijesooriya <anjana.wijesooriya2000@gmail.com>
2022-04-16 23:04:18 +0530

    navbar updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -53,7 +53,7 @@ const NavBar = () => {
               </li>
             ))}
             <div className="flex space-x-6">
-              {pathname === "/" || pathname === "/login" ? (
+              {pathname === "/" || pathname === "/login" || pathname === "/contact" || pathname === "/services" || pathname === "/about" ? (
                 <Link to="/login">
                   <button className="inline-flex items-center bg-red-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-black rounded-full text-base mt-4 md:mt-0 translate-x-6">
                     Login
