cab7f0f4f20f3e4db543b510633f84e745da7a75
SumeeraMadushanka <sumeeramadushanka104@gmail.com>
2022-05-08 16:49:24 +0530

    coding , route

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
+const User = require("../models/Auth");
 
-const { register, login, forgotPassword, resetpassword, registerStaff } = require("../controllers/Auth");
+const {
+  register,
+  login,
+  forgotPassword,
+  resetpassword,
+  registerStaff,
+} = require("../controllers/Auth");
 
 //below routes map the controllers
 router.route("/register").post(register); //call the auth in controllers
@@ -12,5 +19,26 @@ router.route("/forgotpassword").post(forgotPassword);
 router.route("/passwordreset/:resetToken").put(resetpassword);
 
 router.route("/registerStaff").post(registerStaff);
+
+router.route("/get/:id").get(async (req, res) => {
+  const { id } = req.params;
+
+  await User.findById(id)
+    .then((user) => res.json(user))
+    .catch((error) => res.status(500).json({ success: false, error: error }));
+});
+
+router.route("/update/:id").put(async (req, res) => {
+  //backend route for updating relavant data and passing back
+  const { id } = req.params;
+  const { username } = req.body;
+
+  //find the document by and update the relavant data
+  await Customer.findByIdAndUpdate(id, {
+    username,
+  })
+    .then(() => res.json({ success: true }))
+    .catch((error) => res.json({ success: false, error: error }));
+});
 
 module.exports = router;
